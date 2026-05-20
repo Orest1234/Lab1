498912aae6f7c6d803fa69d884a4e85635f8f081
Orest Khimchynskyi <konerd12311@gmail.com>
2026-05-20 03:01:35 +0300

    Lab 7: Jenkins Pipeline with Terraform + Ansible

---
+++ lab7/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = true
+}
+
+resource "docker_container" "web_server" {
+  name  = "jenkins-managed-node"
+  image = docker_image.nginx.image_id
+  ports {
+    internal = 80
+    external = 8095
+  }
+}
+
+output "server_ip" {
+  value = "localhost"
+}
+
+output "server_port" {
+  value = "8095"
+}
